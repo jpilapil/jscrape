596f2418809dc89add02422c06c3caa847bc2d6c
Justin Pilapil <justin.pilapil@yahoo.com>
2023-02-25 18:56:28 -0800

    delete node-fetch

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const { jscrape } = require("./scraper/jscrape");
 
 console.log("Initializing");
 
+// Initialize app
 const interval = async () => {
-  setInterval(jscrape, 1 * 10 * 1000); // 1 sec = 1000
+  setInterval(jscrape, 10 * 60 * 1000); // 1 sec = 1000
 };
 
-// Run jscrape when file is executed, and then in an interval
+// run jscrape when file is executed, and then in an interval
 jscrape();
 interval();
