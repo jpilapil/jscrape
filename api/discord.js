8b2612d41625209cea4fc623408c37aa0f3dc668
Justin Pilapil <justin.pilapil@yahoo.com>
2023-03-01 22:18:20 -0800

    add status info

--- api/discord.js
+++ api/discord.js
@@ -1,5 +1,10 @@
 const dotenv = require("dotenv");
-const { Client, Events, GatewayIntentBits } = require("discord.js");
+const {
+  Client,
+  Events,
+  GatewayIntentBits,
+  ActivityType,
+} = require("discord.js");
 dotenv.config();
 const token = process.env.DISCORD_BOT_TOKEN;
 const channelId = process.env.DISCORD_CHANNEL_ID;
@@ -7,12 +12,20 @@ const channelId = process.env.DISCORD_CHANNEL_ID;
 const sendDiscordMessage = (item) => {
   // Create a new client instance
   const client = new Client({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.GuildMessageReactions,
+    ],
   });
+
   // When the client is ready, run this code (only once)
   // We use 'c' for the event parameter to keep it separate from the already defined 'client'
   client.once(Events.ClientReady, (c) => {
-    //  console.log(`Ready! Logged in as ${c.user.tag}`);
+    // Sets the "Playing status"
+    client.user.setActivity(`for ${item.productName}`, {
+      type: ActivityType.Watching,
+    });
     // Get the channel we want to interact with
     const channel = client.channels.cache.get(channelId);
 
@@ -22,6 +35,7 @@ const sendDiscordMessage = (item) => {
       SKU: ${item.sku}\n
       Price: $${item.price}\n`);
   });
+
   // Log in to Discord with your client's token
   client.login(token);
 };
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ console.log("Initializing n.n");
 jscrape();
 
 // Run jscrape when file is executed, and then in an interval
-const interval = setInterval(jscrape, 1 * 60 * 1000); // 1 sec = 1000
+const interval = setInterval(jscrape, 1 * 10 * 1000); // 1 sec = 1000
 
 // Stop the interval when program is killed
 process.on("SIGINT", () => {
